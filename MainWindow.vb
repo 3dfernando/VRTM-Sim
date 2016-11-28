41373c59a1bf37bbed4d03a6266c796ec717eb79
3dfernando <fzigunov@gmail.com>
2016-11-28 16:13:47 -0200

    Developed Interface Sketch

--- MainWindow.vb
+++ MainWindow.vb
@@ -1,3 +1,172 @@
 ﻿Public Class MainWindow
+#Region "Declares and Private Variables"
+    Dim PrevMidPanelSize As System.Drawing.Size
+    Dim FormShown As Boolean = False
+#End Region
+
+#Region "Initialization"
+    Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub MainWindow_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        'After shown
+        PrevMidPanelSize = MidPanel.Size
+        FormShown = True
+        Me.WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub Init_DGV(RowNumber As Integer, ColNumber As Integer)
+        'Initializes VRTM table
+        Dim i As Integer
+        Dim DefFont As New Font("Calibri", 8, FontStyle.Regular)
+        Dim pad As New Padding(1)
+
+        VRTMTable.BackgroundColor = Color.LightGray
+        VRTMTable.ColumnHeadersDefaultCellStyle.Font = DefFont
+        VRTMTable.ColumnHeadersHeight = 20
+        VRTMTable.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        VRTMTable.ColumnHeadersDefaultCellStyle.Padding = pad
+        VRTMTable.RowHeadersDefaultCellStyle.Font = DefFont
+        VRTMTable.RowHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.TopLeft
+        VRTMTable.RowHeadersDefaultCellStyle.Padding = pad
+        VRTMTable.RowHeadersWidth = 60
+        VRTMTable.DefaultCellStyle.Font = DefFont
+        VRTMTable.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+
+        For i = 1 To ColNumber
+            VRTMTable.Columns.Add(Str(i), Str(i))
+        Next
+        Redim_Width_DGV()
+
+        VRTMTable.Rows.Add(RowNumber)
+        For i = 1 To RowNumber
+            VRTMTable.Rows(i - 1).HeaderCell.Value = Str(i)
+        Next
+    End Sub
+#End Region
+
+
+#Region "Layout Redimensioning Handling"
+    Private Sub Divisor2_SplitterMoved(sender As Object, e As SplitterEventArgs) Handles Divisor2.SplitterMoved
+        'Handles the datagridview column resizing
+        Redim_Width_DGV()
+
+        'Redims the scrollbar and repositions the labels
+        Dim DeltaW As Integer
+        DeltaW = MidPanel.Width - PrevMidPanelSize.Width
+        hsSimPosition.Width = hsSimPosition.Width + DeltaW
+        lblCurrentPos.Left = lblCurrentPos.Left + DeltaW
+        PrevMidPanelSize = MidPanel.Size
+    End Sub
+
+    Private Sub MainWindow_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If FormShown Then
+            'Handles the datagridview column resizing
+            Redim_Width_DGV()
+
+            'Redims the scrollbar and repositions the labels
+            Dim DeltaW As Integer
+            DeltaW = MidPanel.Width - PrevMidPanelSize.Width
+            hsSimPosition.Width = hsSimPosition.Width + DeltaW
+            lblCurrentPos.Left = lblCurrentPos.Left + DeltaW
+            PrevMidPanelSize = MidPanel.Size
+        End If
+    End Sub
+
+    Private Sub Redim_Width_DGV()
+        'Redims the column width according to the new DGV total width and the bounds set in the constants
+        Try
+            Const TableMinColumnWidth As Integer = 40
+            Const TableMaxColumnWidth As Integer = 150
+            Dim ColWidth As Integer
+            ColWidth = Int((VRTMTable.Width - VRTMTable.RowHeadersWidth - 25) / VRTMTable.Columns.Count) '25 accounts for a possible vscrollbar
+
+            If ColWidth < TableMinColumnWidth Then ColWidth = TableMinColumnWidth
+            If ColWidth > TableMaxColumnWidth Then ColWidth = TableMaxColumnWidth
+
+            For i = 1 To VRTMTable.Columns.Count
+                VRTMTable.Columns(i - 1).Width = ColWidth
+            Next
+        Catch ex As Exception
+
+        End Try
+    End Sub
+#End Region
+
+
+#Region "Textbox Units Attachment"
+
+#End Region
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cmdHLEdit.Click
+        Dim A(,) As String
+        Dim i, j As Integer
+
+        ReDim A(9, 4)
+        For i = 0 To UBound(A, 1)
+            For j = 0 To UBound(A, 2)
+                A(i, j) = Str((i + 1) * (j + 1))
+            Next
+        Next
+
+        RefreshTable(A)
+    End Sub
+
+
+    Private Sub RefreshTable(Data(,))
+        If (VRTMTable.Rows.Count <> (UBound(Data, 2) + 1)) Or (VRTMTable.Columns.Count <> (UBound(Data, 1) + 1)) Then
+            Exit Sub
+        End If
+
+        For i = 0 To UBound(Data, 1)
+            For j = 0 To UBound(Data, 2)
+                VRTMTable.Item(i, j).Value = Data(i, j)
+            Next
+        Next
+
+    End Sub
+
+    Private Sub RunProcessSimulationToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RunProcessSimulationToolStripMenuItem.Click
+        Init_DGV(2, 2)
+    End Sub
+
+#Region "Textbox Text Change Subs"
+    Private Sub Validate_Positive(ByVal sender As Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles _
+        txtNLevels.Validating, txtNTrays.Validating, txtBoxesPerTray.Validating, txtFanFlow.Validating, txtEvapSurf.Validating, txtGlobalHX.Validating
+        'Validates several textboxes that have a numeric input
+        If Not (IsNumeric(sender.Text) And Val(sender.text) > 0) Then
+            ' Cancel the event and select the text to be corrected by the user.
+            e.Cancel = True
+            TextBox1.Select(0, TextBox1.Text.Length)
+
+            ' Set the ErrorProvider error with the text to display. 
+            ErrorProvider1.SetError(sender, "A numeric value must be provided.")
+        End If
+    End Sub
+
+    Private Sub Validated_Textbox_VRTM(ByVal sender As Object, ByVal e As System.EventArgs) Handles _
+        txtNLevels.Validated, txtNTrays.Validated, txtBoxesPerTray.Validated
+        ' If all conditions have been met, clear the error provider of errors.
+        ErrorProvider1.SetError(sender, "")
+
+        VRTM_SimVariables.nLevels = Int(txtNLevels.Text)
+        VRTM_SimVariables.nTrays = Int(txtNTrays.Text)
+        VRTM_SimVariables.boxesPerTray = Int(txtBoxesPerTray.Text)
+        VRTM_SimVariables.staticCapacity = VRTM_SimVariables.nLevels * VRTM_SimVariables.nTrays * VRTM_SimVariables.boxesPerTray
+        txtStCap.Text = Trim(Str(VRTM_SimVariables.staticCapacity))
+    End Sub
+
+    Private Sub Validated_Textbox_Evaporators(ByVal sender As Object, ByVal e As System.EventArgs) Handles _
+        txtFanFlow.Validated, txtEvapSurf.Validated, txtGlobalHX.Validated
+        ' If all conditions have been met, clear the error provider of errors.
+        ErrorProvider1.SetError(sender, "")
+
+        VRTM_SimVariables.fanFlowRate = Val(txtFanFlow.Text)
+        VRTM_SimVariables.evapSurfArea = Val(txtEvapSurf.Text)
+        VRTM_SimVariables.globalHX_Coeff = Val(txtGlobalHX.Text)
+    End Sub
+
+#End Region
 
 End Class
---
+++ SimVariables.vb
@@ -0,0 +1,15 @@
+﻿Module SimVariables
+    Public VRTM_SimVariables As New SimulationVariables
+
+    Public Class SimulationVariables
+        Public nTrays As Integer
+        Public nLevels As Integer
+        Public boxesPerTray As Integer
+        Public staticCapacity As Long
+
+        Public fanFlowRate As Double
+        Public evapSurfArea As Double
+        Public globalHX_Coeff As Double
+
+    End Class
+End Module
